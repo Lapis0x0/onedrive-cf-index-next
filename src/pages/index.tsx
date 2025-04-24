680b0a4bf425913e781722a5fb6e4b2f549726be
Lapis0x0 <szy2089931398@gmail.com>
2025-04-24 14:53:18 +0800

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import siteConfig from '../../config/site.config'
 import Navbar from '../components/Navbar'
 import FileListing from '../components/FileListing'
 import Footer from '../components/Footer'
-import Breadcrumb from '../components/Breadcrumb'
-import SwitchLayout from '../components/SwitchLayout'
 
 export default function Home() {
+  const { query } = useRouter()
+  
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
       <Head>
@@ -17,11 +18,7 @@ export default function Home() {
       <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
         <Navbar />
         <div className="mx-auto w-full max-w-5xl py-4 sm:p-4">
-          <nav className="mb-4 flex items-center justify-between px-4 sm:px-0 sm:pl-1">
-            <Breadcrumb />
-            <SwitchLayout />
-          </nav>
-          <FileListing />
+          <FileListing query={query} />
         </div>
       </main>
 
